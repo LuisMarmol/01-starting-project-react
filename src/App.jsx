1e657ff1db49b1eed4e2ddfad964f235ea3d7174
LuisMarmol <luis.marmol@tecnologicoliceocristiano.edu.ec>
2024-05-27 20:32:42 -0500

    Props Corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
+const myFullName = [
+  'Luis', 
+  'Mármol',
+  'Software Developer',
+  '23 Years Old'
+];
+
+
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function MyName({nameProp}) {
+function PersonalData(props) {
   return (
     <div>
-      <p>Nombre: {nameProp[0]}</p>
-      <p>Apellido: {nameProp[1]}</p>
-      <p>Título: {nameProp[2]}</p>
-      <p>Edad: {nameProp[3]}</p>
+      <p>Nombre: {props.nombre}</p>
+      <p>Apellido: {props.apellido}</p>
+      <p>Título: {props.titulo}</p>
+      <p>Edad: {props.edad}</p>
     </div>
   );
 }
@@ -30,18 +38,11 @@ function Header() {
 }
 
 function App() {
-  const myFullName = [
-    'Luis', 
-    'Mármol',
-    'Software Developer',
-    '23 Years Old'
-  ];
-  
   return (
     <div>
       <Header />
       <main>
-        <MyName nameProp={myFullName}/>
+        <PersonalData nombre={myFullName[0]} apellido={myFullName[1]} titulo={myFullName[2]} edad={myFullName[3]}/>
       </main>
     </div>
   );
